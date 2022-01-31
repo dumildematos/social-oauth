dfacc1727e08ad68c389f202f6fc6eebf818a451
Dumilde Matos <dumildejosematos@gmail.com>
2022-01-31 21:23:25 +0100

    first commit

---
+++ src/test/java/com/auth/socialoauth/SocialOauthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.auth.socialoauth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SocialOauthApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
