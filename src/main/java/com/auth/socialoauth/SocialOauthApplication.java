190e0dee72452b00c2f733dc427818b64955ac7a
Dumilde Matos <dumildejosematos@gmail.com>
2022-01-31 21:35:07 +0100

    add configuration

--- src/main/java/com/auth/socialoauth/SocialOauthApplication.java
+++ src/main/java/com/auth/socialoauth/SocialOauthApplication.java
@@ -2,11 +2,25 @@ package com.auth.socialoauth;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.security.Principal;
 
 @SpringBootApplication
 @RestController
 public class SocialOauthApplication {
+
+    @GetMapping
+    public String welcome(){
+        return "Welcome from Google";
+    }
+
+    @GetMapping("/user")
+    public Principal user(Principal principal) {
+        System.out.println("Username" + principal.getName());
+        return principal;
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(SocialOauthApplication.class, args);
